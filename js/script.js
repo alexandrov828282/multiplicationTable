0f26b604895ddba04b0d93c96bfd3c67e112093f
alexandrov828282 <alexandrov828282@gmail.com>
2020-10-13 22:36:27 +0500

    202010132236

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-let multiplier_1 = 1;
-let multiplier_2 = 1;
+let multiplier_1 = 7;
+let multiplier_2 = 5;
 
 let divStr;
 let divCol;
 let elemExpression;
+let elemExpressionIns;
 
 //пропишем пример на странице
-
+elemExpression = document.querySelector("#expression");
+elemExpressionIns = document.createElement("p");
+elemExpressionIns.className = "expression";
+elemExpressionIns.innerHTML = `${multiplier_1} x ${multiplier_2} = `;
+elemExpression.prepend(elemExpressionIns);
 
 //получим контейнер, куда ьудем втавлять
 let elemContainer = document.querySelector("#container");
 
+// let count = 1;
+
 for (let i = 0; i <= multiplier_1; i++) {
     //создадим div.str для вставки
     divStr = document.createElement("div");
-    divStr.className = "str";
+    // divStr.className = "str";
     //вставляем вновь созданный элемент в DOM
     elemContainer.append(divStr);
 
@@ -22,13 +29,24 @@ for (let i = 0; i <= multiplier_1; i++) {
         //создадим div.col для вставки
         divCol = document.createElement("div");
 
-        if (j == 0 || i == 0) {
-            divCol.className = "index-cell";
-            if (j > 0 && i == 0) divCol.innerHTML = j;
-            if (j == 0 && i > 0) divCol.innerHTML = i;
+        if (j == 0 && i == 0) {
+            divCol.className = "col-null";
+        }
+        
+        if (j > 0 && i == 0) {
+            divCol.className = "col-vertical";
+            divCol.innerHTML = j;
+        }
 
-        } else {
+        if (j == 0 && i > 0) {
+            divCol.className = "col-horizontal";
+            divCol.innerHTML = i;
+        } 
+        
+        if (j > 0 && i > 0)
+        {
             divCol.className = "col";
+            divCol.innerHTML = i + multiplier_1 * (j-1) ;
         }
 
         //вставляем вновь созданный элемент в DOM
@@ -36,5 +54,27 @@ for (let i = 0; i <= multiplier_1; i++) {
     }
 }
 
+//rectangleSixe(multiplier_1, multiplier_2);
+
 // let div = document.createElement('div');
 // div.className = "alert";
+
+function rectangleSixe(side1, side2) {
+    let elems = document.getElementsByClassName("col");
+
+    elems[0].style.width = "300px";
+
+    if (side1 == 1 || side2 == 1) {
+        for (let elem of elems) {
+            elem.style.width = "300px";
+            elem.style.height = "300px";
+        }
+    }
+
+    if (side1 == 2 || side2 == 2) {
+        for (let elem of elems) {
+            elem.style.width = "200px";
+            elem.style.height = "200px";
+        }
+    }
+}
